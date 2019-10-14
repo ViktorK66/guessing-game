184a2b9c602366373fe05e77ec31cb7ba99342cb
ViktorK66 <55109336+ViktorK66@users.noreply.github.com>
2019-10-14 17:33:21 +0300

    feat: Added class implementation

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,19 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  constructor() {}
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
+  guess() {
+    this.middle = Math.round((this.min + this.max) / 2);
+    return this.middle;
+  }
+  lower() {
+    this.max = this.middle;
+  }
+  greater() {
+    this.min = this.middle;
+  }
 }
 
 module.exports = GuessingGame;
